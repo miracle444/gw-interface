398e5a1d552f3912f60a9f55d5301399d5175935
EtienneDesticourt <etienne.desticourt@gmail.com>
2015-12-23 01:07:56 +0100

    Fix friendlist duplicate entries dictionary crash, added packet sniffer hook

--- GuildWarsInterface/Networking/Protocol/AuthServerMessage.cs
+++ GuildWarsInterface/Networking/Protocol/AuthServerMessage.cs
@@ -2,12 +2,15 @@
 {
         internal enum AuthServerMessage
         {
+             
                 ComputerInfoReply = 1,
                 TransactionSuccessCode = 3,
                 Character = 7,
                 Dispatch = 9,
                 FriendList = 10,
                 UpdateFriendList = 11,
+                Whisper = 12,
+                //FriendListEnd = 5120,
                 AccountPermissions = 17,
                 PlayerStatus = 20,
                 Gui = 22,
