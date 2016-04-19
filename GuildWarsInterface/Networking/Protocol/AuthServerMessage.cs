01d57a7ef470220010108a2403e73928790de43b
EtienneDesticourt <etienne.desticourt@gmail.com>
2016-04-19 10:35:12 +0200

    Removed change not related to friendlist fix

--- GuildWarsInterface/Networking/Protocol/AuthServerMessage.cs
+++ GuildWarsInterface/Networking/Protocol/AuthServerMessage.cs
@@ -8,7 +8,6 @@
                 Dispatch = 9,
                 FriendList = 10,
                 UpdateFriendList = 11,
-                Whisper = 12,
                 AccountPermissions = 17,
                 PlayerStatus = 20,
                 Gui = 22,
